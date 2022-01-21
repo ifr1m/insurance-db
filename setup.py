dd0faff89aad6bdb601121fba93ed4f87a179856
Ionatan Frim <>
2022-01-22 00:36:45 +0200

    include resources

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='insurance-db',
-    version='0.0.2rc1',
+    version='0.0.3rc1',
     packages=find_packages(include=['insurancedb', 'insurancedb.*']),
     url='',
     install_requires=['pdfplumber==0.5.28', 'pandas', 'click>=8.0.1', 'pytesseract==0.3.8', 'pillow==8.4.0',
@@ -11,6 +11,8 @@ setup(
         'console_scripts': ['insurance-db=insurancedb.main:create_db_parallel']
     },
     license='MIT',
+    package_data={'': ['resources/*.*']},
+    include_package_data=True,
     author='ionatanfrim',
     author_email='ionatanfrim@gmail.com',
     description='insurance db from RO insurance pdf files'
